3f4baf84c891d9d036f82472b9d1b8295a50698c
Guilherme Gois <guilherme.gois@gorilainvest.com.br>
2017-09-22 10:21:32 -0300

    Init material branch

---
+++ src/lib/src/component/md/md-date-format.component.ts
@@ -0,0 +1,29 @@
+/* Angular modules */
+import { Component, Input, forwardRef, OnInit } from '@angular/core';
+import { NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
+import { DateAdapter, NativeDateAdapter } from '@angular/material';
+
+/* Own modules */
+import { BaseDateFormatComponent } from '../base-date-format.component';
+
+@Component({
+    selector: 'md-date-format',
+    templateUrl: './md-date-format.component.html',
+    providers: [
+        { provide: NG_VALUE_ACCESSOR, useExisting: forwardRef(() => MdDateFormatComponent), multi: true },
+        { provide: NG_VALIDATORS, useExisting: forwardRef(() => MdDateFormatComponent), multi: true }
+    ]
+})
+export class MdDateFormatComponent extends BaseDateFormatComponent implements OnInit {
+  @Input() locale = 'pt-BR';
+
+  constructor(private dateAdapter: DateAdapter<NativeDateAdapter>) {
+    super();
+  }
+
+  ngOnInit() {
+    super.ngOnInit();
+
+    this.dateAdapter.setLocale(this.locale);
+  }
+}
--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -1,36 +1,60 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import {
+  MdInputModule,
+  MdDatepickerModule,
+  MdNativeDateModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TextMaskModule } from 'angular2-text-mask';
 import { DateFormatComponent } from './component/date-format.component';
 import { DateFormatModel } from './model/date-format.model';
 import { DateFormatValidator } from './validator/date-format.validator';
 
+const CommonImports = [
+  CommonModule,
+  FormsModule,
+  TextMaskModule
+];
+
+const CommonProviders = [
+  DateFormatModel,
+  DateFormatValidator
+];
+
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        TextMaskModule
-    ],
-    declarations: [
-        DateFormatComponent,
-    ],
-    exports: [
-        DateFormatComponent
-    ],
-    providers: [
-        DateFormatModel,
-        DateFormatValidator
-    ]
+  imports: CommonImports,
+  declarations: [DateFormatComponent],
+  exports: [DateFormatComponent],
+  providers: CommonProviders
 })
 export class DateFormatModule {
-    static forRoot(): ModuleWithProviders {
-        return {
-            ngModule: DateFormatModule,
-            providers: [
-                DateFormatModel,
-                DateFormatValidator
-            ]
-        };
-    }
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: DateFormatModule,
+      providers: CommonProviders
+    };
+  }
+}
+
+@NgModule({
+  imports: [
+    ...CommonImports,
+    BrowserAnimationsModule,
+    MdInputModule,
+    MdDatepickerModule,
+    MdNativeDateModule
+  ],
+  declarations: [DateFormatComponent],
+  exports: [DateFormatComponent],
+  providers: CommonProviders
+})
+export class MdDateFormatModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: MdDateFormatModule,
+      providers: CommonProviders
+    };
+  }
 }
