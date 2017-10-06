ddbcfb4021cfba9a522599acca39f36b18b5fd0c
Gois <guijocargo@gmail.com>
2017-10-06 13:56:10 -0300

    Change prefix md to mat

--- src/lib/src/module.ts
+++ src/lib/src/module.ts
@@ -7,9 +7,9 @@ import {
   MatNativeDateModule
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TextMaskModule } from 'angular2-text-mask/src/angular2TextMask';
+import { TextMaskModule } from 'angular2-text-mask';
 import { DateFormatComponent } from './component/date-format.component';
-import { MdDateFormatComponent } from './component/md/md-date-format.component';
+import { MatDateFormatComponent } from './component/mat/mat-date-format.component';
 import { DateFormatModel } from './model/date-format.model';
 import { DateFormatValidator } from './validator/date-format.validator';
 
@@ -48,14 +48,14 @@ export class DateFormatModule {
     MatDatepickerModule,
     MatNativeDateModule
   ],
-  declarations: [MdDateFormatComponent],
-  exports: [MdDateFormatComponent],
+  declarations: [MatDateFormatComponent],
+  exports: [MatDateFormatComponent],
   providers: CommonProviders
 })
-export class MdDateFormatModule {
+export class MatDateFormatModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: MdDateFormatModule,
+      ngModule: MatDateFormatModule,
       providers: CommonProviders
     };
   }
