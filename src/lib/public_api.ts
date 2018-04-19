2982a08ccacc7501e3179430ac538ea25acaceef
Gois <guijocargo@gmail.com>
2018-04-19 18:00:34 -0300

    Export mock files within library

--- src/lib/public_api.ts
+++ src/lib/public_api.ts
@@ -1,5 +1,7 @@
 export * from './src/component/date-format.component';
 export * from './src/component/mat/mat-date-format.component';
+export * from './src/mock/date-format.model.mock';
+export * from './src/mock/date-format.validator.mock';
 export * from './src/model/date-format.enum';
 export * from './src/model/date-format.model';
 export * from './src/validator/date-format.validator';
